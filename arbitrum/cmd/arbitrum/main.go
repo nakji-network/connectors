15201e63bce170caf68a038856059e64ea69bc09
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2022-06-03 01:33:08 -0700

    arbitrum

---
+++ arbitrum/cmd/arbitrum/main.go
@@ -0,0 +1,27 @@
+// This connector ingests real time data from any EVM compatible chain.
+package main
+
+import (
+	"github.com/nakji-network/connector"
+	"github.com/nakji-network/connectors/ethereum"
+	"github.com/nakji-network/connectors/ethereum/evm"
+)
+
+func main() {
+	c := connector.NewConnector()
+
+	// Register topic and protobuf type mappings
+	c.RegisterProtos(
+		&evm.Block{},
+		&evm.Transaction{},
+	)
+
+	evmConnector := ethereum.EthereumConnector{
+		Connector: c,
+
+		// Any additional command line arguments, such as chain selection override. Set up via https://pkg.go.dev/github.com/spf13/viper#readme-working-with-flags
+		Chain: "arbitrum",
+	}
+
+	evmConnector.Start()
+}
