4d8a59e56484a30588548620811c5e7a2d719e21
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2022-06-06 01:12:49 -0700

    gnosis, oec

---
+++ gnosis/cmd/gnosis/main.go
@@ -0,0 +1,30 @@
+// This connector ingests real time data from any EVM compatible chain.
+package main
+
+import (
+	"github.com/nakji-network/connector"
+	"github.com/nakji-network/connectors/ethereum"
+	"github.com/nakji-network/connectors/ethereum/evm"
+)
+
+func main() {
+	c := connector.NewConnector()
+
+	// Register topic and protobuf type mappings
+	c.RegisterProtos(
+		&evm.Block{},
+		&evm.Transaction{},
+	)
+
+	evmConnector := ethereum.EthereumConnector{
+		Connector: c,
+
+		// Any additional command line arguments, such as chain selection override. Set up via https://pkg.go.dev/github.com/spf13/viper#readme-working-with-flags
+		Chain: "gnosis",
+
+		//https://njkhanh.com/go-ethereum-ethclient-provides-wrong-blockhash-on-xdai-p5f3136343038
+		GetBlockByNumber: true,
+	}
+
+	evmConnector.Start()
+}
