67ddbc97803d0114d3ec0fda4cf5d6a05d0b8c0c
Hung Doan Phi <hugjobk@gmail.com>
2022-09-30 14:40:46 +0700

    [EN-1508] Improve latency (#78)
    * Improve latency
    
    * Sort Blocks and Transactions
    
    * Add fields to Block
    
    Co-authored-by: Doan Phi Hung <davidhung@blep.ai>

--- flow/subscription.go
+++ flow/subscription.go
@@ -17,6 +17,7 @@ import (
 	flowgrpc "github.com/onflow/flow-go-sdk/access/grpc"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -219,77 +220,160 @@ func (s *Subscription) getEvents(startHeight uint64, endHeight uint64) {
 }
 
 func (s *Subscription) getBlocks(startHeight uint64, endHeight uint64) {
+	var (
+		txMtx        sync.Mutex
+		blockMtx     sync.Mutex
+		transactions []*Transaction
+		blocks       = make([]*Block, 0, endHeight-startHeight+1)
+	)
+	wpSize1 := endHeight - startHeight + 1
+	if wpSize1 > 10 {
+		wpSize1 = 10 // Limit WorkerPool size
+	}
+	wp1 := NewWorkerPool(int(wpSize1))
 	for height := startHeight; height <= endHeight; height++ {
-		block, err := s.getBlockByHeight(height)
-		if err != nil {
-			s.errChan <- err
-			continue
-		}
-		s.blockChan <- &Block{
-			Ts:       timestamppb.New(block.Timestamp),
-			Id:       block.ID[:],
-			ParentID: block.ParentID[:],
-			Height:   block.Height,
-		}
-		for i := range block.CollectionGuarantees {
-			collID := block.CollectionGuarantees[i].CollectionID
-			ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-			coll, err := s.grpc.GetCollection(ctx, collID)
-			cancel()
+		wp1.Run(func() {
+			// Get Block
+			block, err := s.getBlockByHeight(height)
 			if err != nil {
 				s.errChan <- err
-				continue
+				return
 			}
-			for _, txID := range coll.TransactionIDs {
-				ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-				tx, err := s.grpc.GetTransaction(ctx, txID)
-				cancel()
-				if err != nil {
-					s.errChan <- err
-					continue
-				}
-				transaction := Transaction{
-					Ts:               timestamppb.New(block.Timestamp),
-					Id:               txID[:],
-					BlockID:          block.ID[:],
-					CollectionID:     collID[:],
-					Script:           tx.Script,
-					Arguments:        tx.Arguments,
-					ReferenceBlockID: tx.ReferenceBlockID[:],
-					GasLimit:         tx.GasLimit,
-					ProposalKey: &ProposalKey{
-						Address:        tx.ProposalKey.Address[:],
-						KeyIndex:       int64(tx.ProposalKey.KeyIndex),
-						SequenceNumber: tx.ProposalKey.SequenceNumber,
-					},
-					Payer:              tx.Payer[:],
-					Authorizers:        make([][]byte, 0, len(tx.Authorizers)),
-					PayloadSignatures:  make([]*TransactionSignature, 0, len(tx.PayloadSignatures)),
-					EnvelopeSignatures: make([]*TransactionSignature, 0, len(tx.EnvelopeSignatures)),
-				}
-				for _, authorizers := range tx.Authorizers {
-					transaction.Authorizers = append(transaction.Authorizers, authorizers[:])
-				}
-				for _, sig := range tx.PayloadSignatures {
-					transaction.PayloadSignatures = append(transaction.PayloadSignatures, &TransactionSignature{
-						Address:     sig.Address[:],
-						SignerIndex: int64(sig.SignerIndex),
-						KeyIndex:    int64(sig.KeyIndex),
-						Signature:   sig.Signature,
-					})
-				}
-				for _, sig := range tx.EnvelopeSignatures {
-					transaction.EnvelopeSignatures = append(transaction.EnvelopeSignatures, &TransactionSignature{
-						Address:     sig.Address[:],
-						SignerIndex: int64(sig.SignerIndex),
-						KeyIndex:    int64(sig.KeyIndex),
-						Signature:   sig.Signature,
-					})
-				}
-				s.transactionChan <- &transaction
+			b := &Block{
+				Ts:                   timestamppb.New(block.Timestamp),
+				Id:                   block.ID[:],
+				ParentID:             block.ParentID[:],
+				Height:               block.Height,
+				CollectionGuarantees: make([]*CollectionGuarantee, 0, len(block.CollectionGuarantees)),
+				Seals:                make([]*BlockSeal, 0, len(block.Seals)),
 			}
-		}
+			for _, collectionGuarantee := range block.CollectionGuarantees {
+				b.CollectionGuarantees = append(b.CollectionGuarantees, &CollectionGuarantee{
+					CollectionID: collectionGuarantee.CollectionID[:],
+				})
+			}
+			for _, seal := range block.Seals {
+				b.Seals = append(b.Seals, &BlockSeal{
+					BlockID:            seal.BlockID[:],
+					ExecutionReceiptID: seal.ExecutionReceiptID[:],
+				})
+			}
+			blockMtx.Lock()
+			blocks = append(blocks, b)
+			blockMtx.Unlock()
+			// Get Transactions for Block
+			wpSize2 := len(block.CollectionGuarantees)
+			if wpSize2 > 5 {
+				wpSize2 = 5 // Limit WorkerPool size
+			}
+			wp2 := NewWorkerPool(wpSize2)
+			for i := range block.CollectionGuarantees {
+				collID := block.CollectionGuarantees[i].CollectionID
+				wp2.Run(func() {
+					txs := s.getTransactions(block, collID)
+					txMtx.Lock()
+					transactions = append(transactions, txs...)
+					txMtx.Unlock()
+				})
+			}
+			wp2.Wait()
+		})
 	}
+	wp1.Wait()
+	// Sort blocks by height
+	sort.Slice(blocks, func(i, j int) bool {
+		return blocks[i].Height < blocks[j].Height
+	})
+	// Sort transactions by timestamp and collection ID
+	sort.Slice(transactions, func(i, j int) bool {
+		tx1 := transactions[i]
+		tx2 := transactions[j]
+		return tx1.Ts.AsTime().Before(tx2.Ts.AsTime()) || (tx1.Ts.AsTime().Equal(tx2.Ts.AsTime()) && string(tx1.CollectionID) < string(tx2.CollectionID))
+	})
+	for _, block := range blocks {
+		s.blockChan <- block
+	}
+	for _, transaction := range transactions {
+		s.transactionChan <- transaction
+	}
+}
+
+func (s *Subscription) getTransactions(block *flow.Block, collID flow.Identifier) []*Transaction {
+	coll, err := s.getCollection(collID)
+	if err != nil {
+		s.errChan <- err
+		return nil
+	}
+	var mtx sync.Mutex
+	transactions := make([]*Transaction, 0, len(coll.TransactionIDs))
+	wpSize := len(coll.TransactionIDs)
+	if wpSize > 10 {
+		wpSize = 10 // Limit WorkerPool size
+	}
+	wp := NewWorkerPool(wpSize)
+	for i := range coll.TransactionIDs {
+		txID := coll.TransactionIDs[i]
+		wp.Run(func() {
+			transaction, err := s.getTransaction(block, collID, txID)
+			if err != nil {
+				s.errChan <- err
+				return
+			}
+			mtx.Lock()
+			transactions = append(transactions, transaction)
+			mtx.Unlock()
+		})
+	}
+	wp.Wait()
+	return transactions
+}
+
+func (s *Subscription) getTransaction(block *flow.Block, collID flow.Identifier, txID flow.Identifier) (*Transaction, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	tx, err := s.grpc.GetTransaction(ctx, txID)
+	cancel()
+	if err != nil {
+		return nil, err
+	}
+	transaction := Transaction{
+		Ts:               timestamppb.New(block.Timestamp),
+		Id:               txID[:],
+		BlockID:          block.ID[:],
+		CollectionID:     collID[:],
+		Script:           tx.Script,
+		Arguments:        tx.Arguments,
+		ReferenceBlockID: tx.ReferenceBlockID[:],
+		GasLimit:         tx.GasLimit,
+		ProposalKey: &ProposalKey{
+			Address:        tx.ProposalKey.Address[:],
+			KeyIndex:       int64(tx.ProposalKey.KeyIndex),
+			SequenceNumber: tx.ProposalKey.SequenceNumber,
+		},
+		Payer:              tx.Payer[:],
+		Authorizers:        make([][]byte, 0, len(tx.Authorizers)),
+		PayloadSignatures:  make([]*TransactionSignature, 0, len(tx.PayloadSignatures)),
+		EnvelopeSignatures: make([]*TransactionSignature, 0, len(tx.EnvelopeSignatures)),
+	}
+	for _, authorizers := range tx.Authorizers {
+		transaction.Authorizers = append(transaction.Authorizers, authorizers[:])
+	}
+	for _, sig := range tx.PayloadSignatures {
+		transaction.PayloadSignatures = append(transaction.PayloadSignatures, &TransactionSignature{
+			Address:     sig.Address[:],
+			SignerIndex: int64(sig.SignerIndex),
+			KeyIndex:    int64(sig.KeyIndex),
+			Signature:   sig.Signature,
+		})
+	}
+	for _, sig := range tx.EnvelopeSignatures {
+		transaction.EnvelopeSignatures = append(transaction.EnvelopeSignatures, &TransactionSignature{
+			Address:     sig.Address[:],
+			SignerIndex: int64(sig.SignerIndex),
+			KeyIndex:    int64(sig.KeyIndex),
+			Signature:   sig.Signature,
+		})
+	}
+	return &transaction, nil
 }
 
 func (s *Subscription) getLogs(startHeight uint64, endHeight uint64) {
@@ -322,7 +406,7 @@ func (s *Subscription) getLogs(startHeight uint64, endHeight uint64) {
 			}
 		}
 	}
-	// Sort logs based on block height, transaction index and event index
+	// Sort logs by block height, transaction index and event index
 	sort.Slice(logs, func(i, j int) bool {
 		log1 := logs[i]
 		log2 := logs[j]
@@ -376,5 +460,26 @@ func (s *Subscription) getEventsForHeightRange(eventType string, startHeight uin
 		ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
 		defer cancel()
 		return s.grpc.GetEventsForBlockIDs(ctx, eventType, blockIDs)
+	}
+}
+
+func (s *Subscription) getCollection(collID flow.Identifier) (*flow.Collection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			coll, err := s.grpc.GetCollection(ctx, collID)
+			if err != nil {
+				if rpcErr, ok := err.(flowgrpc.RPCError); ok && rpcErr.GRPCStatus().Code() == codes.NotFound {
+					time.Sleep(1 * time.Second) // Retry after 1s
+					continue
+				}
+				return nil, err
+			}
+			return coll, nil
+		}
 	}
 }
