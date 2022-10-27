c43e959589e8ca1995662022ebdde95fa7f4e8dc
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2022-10-28 00:48:23 +0900

    add woofi contracts

--- woofi/cmd/woofi/main.go
+++ woofi/cmd/woofi/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/nakji-network/connector/config"
 	"github.com/nakji-network/connectors/woofi"
 	"github.com/nakji-network/connectors/woofi/WooPP"
+	"github.com/nakji-network/connectors/woofi/WooPPV1"
+	"github.com/nakji-network/connectors/woofi/WooPPV2"
+	"github.com/nakji-network/connectors/woofi/WooRouterV1"
+	"github.com/nakji-network/connectors/woofi/WooRouterV2"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/pflag"
@@ -38,7 +42,53 @@ func main() {
 		&WooPP.Withdraw{},
 		&WooPP.WooGuardianUpdated{},
 		&WooPP.WooracleUpdated{},
-		&WooPP.WooSwap{})
+		&WooPP.WooSwap{},
+
+		&WooPP.FeeManagerUpdated{},
+		&WooPP.OwnershipTransferPrepared{},
+		&WooPP.OwnershipTransferred{},
+		&WooPP.ParametersUpdated{},
+		&WooPP.Paused{},
+		&WooPP.RewardManagerUpdated{},
+		&WooPP.StrategistUpdated{},
+		&WooPP.Unpaused{},
+		&WooPP.Withdraw{},
+		&WooPP.WooGuardianUpdated{},
+		&WooPP.WooracleUpdated{},
+		&WooPP.WooSwap{},
+
+		&WooPPV1.OwnershipTransferPrepared{},
+		&WooPPV1.OwnershipTransferred{},
+		&WooPPV1.ParametersUpdated{},
+		&WooPPV1.Paused{},
+		&WooPPV1.RewardManagerUpdated{},
+		&WooPPV1.StrategistUpdated{},
+		&WooPPV1.Unpaused{},
+		&WooPPV1.Withdraw{},
+		&WooPPV1.WooGuardianUpdated{},
+		&WooPPV1.WooracleUpdated{},
+		&WooPPV1.WooSwap{},
+
+		&WooPPV2.OwnershipTransferPrepared{},
+		&WooPPV2.OwnershipTransferred{},
+		&WooPPV2.ParametersUpdated{},
+		&WooPPV2.Paused{},
+		&WooPPV2.RewardManagerUpdated{},
+		&WooPPV2.StrategistUpdated{},
+		&WooPPV2.Unpaused{},
+		&WooPPV2.Withdraw{},
+		&WooPPV2.WooGuardianUpdated{},
+		&WooPPV2.WooracleUpdated{},
+		&WooPPV2.WooSwap{},
+
+		&WooRouterV1.OwnershipTransferred{},
+		&WooRouterV1.WooPoolChanged{},
+		&WooRouterV1.WooRouterSwap{},
+
+		&WooRouterV2.OwnershipTransferred{},
+		&WooRouterV2.WooPoolChanged{},
+		&WooRouterV2.WooRouterSwap{},
+	)
 
 	conf := &woofi.Config{
 		NetworkName: "bsc",
