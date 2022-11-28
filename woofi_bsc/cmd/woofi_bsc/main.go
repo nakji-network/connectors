59d8ce0ca0e393b9b89b4b919df6a3158f32472f
Doan Phi Hung <davidhung@blep.ai>
2022-11-28 17:52:16 +0700

    Import from base woofi

--- woofi_bsc/cmd/woofi_bsc/main.go
+++ woofi_bsc/cmd/woofi_bsc/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/nakji-network/connectors/woofi_bsc"
-	"github.com/nakji-network/connectors/woofi_bsc/WooCrossChainRouterV1"
-	"github.com/nakji-network/connectors/woofi_bsc/WooPPV2"
-	"github.com/nakji-network/connectors/woofi_bsc/WooPPV3"
-	"github.com/nakji-network/connectors/woofi_bsc/WooRouterV1"
-	"github.com/nakji-network/connectors/woofi_bsc/WooRouterV2"
+	"github.com/nakji-network/woofi-connectors/woofi"
+	"github.com/nakji-network/woofi-connectors/woofi/WooCrossChainRouterV1"
+	"github.com/nakji-network/woofi-connectors/woofi/WooPPV2"
+	"github.com/nakji-network/woofi-connectors/woofi/WooPPV3"
+	"github.com/nakji-network/woofi-connectors/woofi/WooRouterV1"
+	"github.com/nakji-network/woofi-connectors/woofi/WooRouterV2"
 
 	"github.com/spf13/pflag"
 	_ "go.uber.org/automaxprocs"
@@ -18,13 +18,13 @@ func main() {
 
 	pflag.Parse()
 
-	conf := &woofi_bsc.Config{
+	conf := &woofi.Config{
 		NetworkName: "bsc",
 		FromBlock:   *fromBlock,
 		NumBlocks:   *numBlocks,
 	}
 
-	c := woofi_bsc.New(conf)
+	c := woofi.New(conf)
 	c.AddContract(WooPPV2.NewContract("0x10C24658815585851a8888f059Cb4338790023F1"))               // WooPPV1
 	c.AddContract(WooPPV2.NewContract("0x8489d142Da126F4Ea01750e80ccAa12FD1642988"))               // WooPPV2
 	c.AddContract(WooPPV3.NewContract("0xbf365Ce9cFcb2d5855521985E351bA3bcf77FD3F"))               // WooPPV3
