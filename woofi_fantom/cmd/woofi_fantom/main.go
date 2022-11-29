4090bc051c2d2f4be7b06b431cea478c1eef1235
Hung Doan Phi <hugjobk@gmail.com>
2022-11-29 09:27:16 +0700

    Import from base woofi (#133)
    Co-authored-by: Doan Phi Hung <davidhung@blep.ai>

--- woofi_fantom/cmd/woofi_fantom/main.go
+++ woofi_fantom/cmd/woofi_fantom/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/nakji-network/connectors/woofi_fantom"
-	"github.com/nakji-network/connectors/woofi_fantom/WooCrossChainRouterV1"
-	"github.com/nakji-network/connectors/woofi_fantom/WooPPV3"
-	"github.com/nakji-network/connectors/woofi_fantom/WooRouterV2"
+	"github.com/nakji-network/woofi-connectors/woofi"
+	"github.com/nakji-network/woofi-connectors/woofi/WooCrossChainRouterV1"
+	"github.com/nakji-network/woofi-connectors/woofi/WooPPV3"
+	"github.com/nakji-network/woofi-connectors/woofi/WooRouterV2"
 
 	"github.com/spf13/pflag"
 	_ "go.uber.org/automaxprocs"
@@ -16,13 +16,13 @@ func main() {
 
 	pflag.Parse()
 
-	conf := &woofi_fantom.Config{
+	conf := &woofi.Config{
 		NetworkName: "fantom",
 		FromBlock:   *fromBlock,
 		NumBlocks:   *numBlocks,
 	}
 
-	c := woofi_fantom.New(conf)
+	c := woofi.New(conf)
 	c.AddContract(WooPPV3.NewContract("0x9503E7517D3C5bc4f9E4A1c6AE4f8B33AC2546f2"))               // WooPPV3
 	c.AddContract(WooRouterV2.NewContract("0x37B5a5A730dAD670874f26Cc5507bb1b9705e447"))           // WooRouterV2
 	c.AddContract(WooCrossChainRouterV1.NewContract("0xcF6Ce5Fd6bf28bB1AeAc88A55251f6c840059De5")) // WooCrossChainRouterV1
